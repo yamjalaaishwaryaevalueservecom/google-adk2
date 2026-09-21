e9c19d5bcbd92057489cc8abd5f02569b9866b65
yamjalaaishwaryaevalueservecom <yamjala.aishwarya@evalueserve.com>
2026-09-21 07:01:39 +0000

    Second commit

--- agent.py
+++ agent.py
@@ -1,8 +1,9 @@
 from google.adk.agents.llm_agent import Agent
+import pandas as pd
 
 root_agent = Agent(
     model='gemini-3.5-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    instruction='Answer user questions to the best of your knowledge. Keep with answers crisp',
 )
